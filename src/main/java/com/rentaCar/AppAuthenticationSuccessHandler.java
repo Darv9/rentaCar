264f4ab3d514cee1f46c0345912cf7904cb9ef7a
Diego Segura Vega <Diego Segura Vega@LAPTOP-TTDFIE71>
2022-08-01 22:30:13 -0600

    Desarrollo del AppAuthenticationSuccessHnadler.

--- src/main/java/com/rentaCar/AppAuthenticationSuccessHandler.java
+++ src/main/java/com/rentaCar/AppAuthenticationSuccessHandler.java
@@ -4,10 +4,21 @@
  */
 package com.rentaCar;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
 /**
  *
  * @author Diego Segura Vega
  */
-public class AppAuthenticationSuccessHandler {
+public class AppAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     
+    protected void handle(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+    }
 }
+
