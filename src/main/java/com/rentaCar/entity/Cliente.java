262dfd1facccd370da3e69b9bfb18422cc495909
Jason Quiros - Ostional <>
2022-08-03 14:44:07 -0600

    Spring boot Cliente / Entity

--- src/main/java/com/rentaCar/entity/Cliente.java
+++ src/main/java/com/rentaCar/entity/Cliente.java
@@ -1,6 +1,9 @@
 package com.rentaCar.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,6 +25,11 @@ public class Cliente implements Serializable{
     private String telefono;
     private String correo;
     private String domicilio;
+   
+   private String password;
+    private int active;
+    private String roles="";
+    private String permissions=""; 
     
     @ManyToOne
     @JoinColumn(name="nacionalidad_id")
@@ -90,4 +98,51 @@ public class Cliente implements Serializable{
     public void setNacionalidad(Nacionalidad nacionalidad) {
         this.nacionalidad = nacionalidad;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
+    }
+
+   public List<String> getRoleList(){
+        if(this.roles.length()>0){
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+                
+    public List<String> getPermissionList(){
+        if(this.permissions.length()>0){
+            return Arrays.asList(this.permissions.split(","));
+        }
+        return new ArrayList<>();
+    }
+        
 }
--- src/main/java/com/rentaCar/repository/DetalleVentaRepository.java
+++ src/main/java/com/rentaCar/repository/DetalleVentaRepository.java
@@ -1,16 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.rentaCar.repository;
 
 import com.rentaCar.entity.DetalleVenta;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *
  * @author Diego Segura Vega
  */
+
+@Repository
 public interface DetalleVentaRepository extends CrudRepository<DetalleVenta,Long> {
     
 }
