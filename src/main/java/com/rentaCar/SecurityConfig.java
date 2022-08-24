58c3ea16e739eda09a20bf99ff11392f38c1c4a3
Darv9 <daniel.rave0903@gmail.com>
2022-08-24 13:50:54 -0600

    Correcion Footer template

--- src/main/java/com/rentaCar/SecurityConfig.java
+++ src/main/java/com/rentaCar/SecurityConfig.java
@@ -57,7 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/clientes","/login","/clienteN", "/detalleRenta", "/detalleRentaN", "/reclamo", "/detalleVentaN", "/empleado", "/empleadoN", "/nacionalidad", "/nacionalidadN")
                 .hasRole("ADMIN")
                 .antMatchers("/clientes", "/","/login")
-                .hasAnyRole("USER")
+                .hasAnyRole("PLAT")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
